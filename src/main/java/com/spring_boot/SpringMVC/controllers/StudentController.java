6c213018188ed79bf517bbed1b3caa605b4322fc
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-09-06 12:17:21 +0530

    Working with different form tags and binding data from application.properties

---
+++ src/main/java/com/spring_boot/SpringMVC/controllers/StudentController.java
@@ -0,0 +1,37 @@
+package com.spring_boot.SpringMVC.controllers;
+
+import com.spring_boot.SpringMVC.pojos.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    //Reading properties value from application.properties file
+    @Value("${countries}")
+    List<String> countries;
+
+    @GetMapping("/registration")
+    public String showRegistrationForm(Model theModel){
+        //Creating an instance of Student Class
+        Student student=new Student();
+        //Adding instance to Model object.
+        theModel.addAttribute("student",student);
+        theModel.addAttribute("countries",countries);
+//        System.out.println(countries);
+        return "registration-form";
+    }
+
+    @PostMapping("/submit-form")
+    public String submitRegistrationForm(@ModelAttribute("student") Student theStudent,Model theModel){
+        theModel.addAttribute("student",theStudent);
+        System.out.println(theStudent);
+        return "submit-registration";
+    }
+}
---
+++ src/main/java/com/spring_boot/SpringMVC/pojos/Student.java
@@ -0,0 +1,51 @@
+package com.spring_boot.SpringMVC.pojos;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String favouriteLanguage;
+
+    public String getFavouriteLanguage() {
+        return favouriteLanguage;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                ", favouriteLanguage='" + favouriteLanguage + '\'' +
+                '}';
+    }
+
+    public void setFavouriteLanguage(String favouriteLanguage) {
+        this.favouriteLanguage = favouriteLanguage;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
